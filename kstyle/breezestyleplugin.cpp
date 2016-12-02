polter-rnd/adwaita-compact-qt

Import the Breeze theme by KDE

kstyle/breezestyleplugin.cpp
@@ -1,0 +1,52 @@
+
+/*************************************************************************
+ * Copyright (C) 2014 by Hugo Pereira Da Costa <hugo.pereira@free.fr>    *
+ *                                                                       *
+ * This program is free software; you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation; either version 2 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program; if not, write to the                         *
+ * Free Software Foundation, Inc.,                                       *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ *************************************************************************/
+
+#include "breezestyleplugin.h"
+#include "breezestyle.h"
+
+#include <QApplication>
+
+#if QT_VERSION < 0x050000
+Q_EXPORT_PLUGIN2( breeze-qt, Breeze::StylePlugin )
+#endif
+
+namespace Breeze
+{
+
+    //_________________________________________________
+    QStyle* StylePlugin::create( const QString &key )
+    {
+        if( key.toLower() == QStringLiteral( "breeze" ) )
+        {
+            return new Style;
+        }
+        return nullptr;
+    }
+
+    //_________________________________________________
+    StylePlugin::~StylePlugin()
+    {
+    }
+
+    //_________________________________________________
+    QStringList StylePlugin::keys() const
+    { return QStringList( QStringLiteral( "Breeze" ) ); }
+
+}

kstyle/breezestyleplugin.h
@@ -1,0 +1,57 @@
+#ifndef breezestyleplugin_h
+#define breezestyleplugin_h
+
+/*************************************************************************
+ * Copyright (C) 2014 by Hugo Pereira Da Costa <hugo.pereira@free.fr>    *
+ *                                                                       *
+ * This program is free software; you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation; either version 2 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program; if not, write to the                         *
+ * Free Software Foundation, Inc.,                                       *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ *************************************************************************/
+
+#include <QStylePlugin>
+
+namespace Breeze
+{
+
+    class StylePlugin : public QStylePlugin
+    {
+
+        Q_OBJECT
+
+        #if QT_VERSION >= 0x050000
+        Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QStyleFactoryInterface" FILE "breeze.json" )
+        #endif
+
+        public:
+
+        //* constructor
+        explicit StylePlugin(QObject *parent = 0):
+            QStylePlugin(parent)
+        {}
+
+        //* destructor
+        ~StylePlugin();
+
+        //* returns list of valid keys
+        QStringList keys() const;
+
+        //* create style
+        QStyle* create( const QString& );
+
+    };
+
+}
+
+#endif

FedoraQt/adwaita-qt

kstyle/breezestyleplugin.cpp
@@ -1,0 +1,52 @@
+
+/*************************************************************************
+ * Copyright (C) 2014 by Hugo Pereira Da Costa <hugo.pereira@free.fr>    *
+ *                                                                       *
+ * This program is free software; you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation; either version 2 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program; if not, write to the                         *
+ * Free Software Foundation, Inc.,                                       *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ *************************************************************************/
+
+#include "breezestyleplugin.h"
+#include "breezestyle.h"
+
+#include <QApplication>
+
+#if QT_VERSION < 0x050000
+Q_EXPORT_PLUGIN2( breeze-qt, Breeze::StylePlugin )
+#endif
+
+namespace Breeze
+{
+
+    //_________________________________________________
+    QStyle* StylePlugin::create( const QString &key )
+    {
+        if( key.toLower() == QStringLiteral( "adwaita" ) )
+        {
+            return new Style;
+        }
+        return nullptr;
+    }
+
+    //_________________________________________________
+    StylePlugin::~StylePlugin()
+    {
+    }
+
+    //_________________________________________________
+    QStringList StylePlugin::keys() const
+    { return QStringList( QStringLiteral( "Adwaita" ) ); }
+
+}

kstyle/breezestyleplugin.h
@@ -1,0 +1,57 @@
+#ifndef breezestyleplugin_h
+#define breezestyleplugin_h
+
+/*************************************************************************
+ * Copyright (C) 2014 by Hugo Pereira Da Costa <hugo.pereira@free.fr>    *
+ *                                                                       *
+ * This program is free software; you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation; either version 2 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program; if not, write to the                         *
+ * Free Software Foundation, Inc.,                                       *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ *************************************************************************/
+
+#include <QStylePlugin>
+
+namespace Breeze
+{
+
+    class StylePlugin : public QStylePlugin
+    {
+
+        Q_OBJECT
+
+        #if QT_VERSION >= 0x050000
+        Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QStyleFactoryInterface" FILE "adwaita.json" )
+        #endif
+
+        public:
+
+        //* constructor
+        explicit StylePlugin(QObject *parent = 0):
+            QStylePlugin(parent)
+        {}
+
+        //* destructor
+        ~StylePlugin();
+
+        //* returns list of valid keys
+        QStringList keys() const;
+
+        //* create style
+        QStyle* create( const QString& );
+
+    };
+
+}
+
+#endif