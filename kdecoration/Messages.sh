9d8b1a41e630aeeaff0930d2090070ebe525f313
Martin Briza <mbriza@redhat.com>
2016-12-13 18:34:34 +0100

    Remove directories that are not built
    (qtquickcontrols and kdecoration)
    
    For now. To be readded in the future

--- kdecoration/Messages.sh
+++
@@ -1,4 +0,0 @@
-#!bin/sh
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/adwaita_kwin_deco.pot
-rm -f rc.cpp
