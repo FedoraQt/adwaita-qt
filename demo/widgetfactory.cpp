1b9705a1e31683fbfc0ed34bdf8d77d963cb3b5b
Jan Grulich <jgrulich@redhat.com>
2019-04-08 13:26:40 +0200

    WidgetFactory: better 1:1 copy of the GTK factory demo app

--- demo/widgetfactory.cpp
+++ demo/widgetfactory.cpp
@@ -25,6 +25,7 @@
 #include "ui_widgetfactory.h"
 
 #include <QAction>
+#include <QLineEdit>
 #include <QMenu>
 #include <QMenuBar>
 #include <QToolBar>
@@ -40,12 +41,55 @@ WidgetFactory::WidgetFactory(QWidget *parent)
     : QMainWindow(parent)
     , d(new WidgetFactory::Private())
 {
-    setGeometry(0, 0, 1024, 768);
+    setGeometry(0, 0, 1440, 800);
 
     QWidget *mainWidget = new QWidget(this);
     d->ui.setupUi(mainWidget);
 
     setCentralWidget(mainWidget);
+
+    // QLineEdit with the icon inside
+    QAction *lineEditAction = new QAction(QIcon::fromTheme(QLatin1String("view-refresh")), QString());
+    connect(lineEditAction, &QAction::triggered, this, [this] (bool checked) {
+        if (d->ui.linedit->echoMode() == QLineEdit::Normal) {
+            d->ui.linedit->setEchoMode(QLineEdit::Password);
+        } else {
+            d->ui.linedit->setEchoMode(QLineEdit::Normal);
+        }
+    });
+    d->ui.linedit->addAction(lineEditAction, QLineEdit::TrailingPosition);
+
+    // QLineEdit with the widget on the right side
+    connect(d->ui.button, &QPushButton::pressed, this, [this] () {
+        d->ui.lineedit2->clear();
+    });
+
+    // Checkboxes
+    d->ui.checkbox3->setCheckState(Qt::PartiallyChecked);
+    d->ui.checkbox6->setCheckState(Qt::PartiallyChecked);
+
+    // Buttons
+    d->ui.pushbutton3->setDown(true);
+    d->ui.pushbutton4->setDown(true);
+
+    // Sliders and progress bars
+    connect(d->ui.horizontalslider3, &QSlider::valueChanged, this, [this] (int value) {
+        d->ui.progressbar_horizontal->setValue(value);
+        d->ui.progressbar_horizontal2->setValue(value);
+        d->ui.progressbar_vertical->setValue(value);
+        d->ui.progressbar_vertical2->setValue(value);
+    });
+
+    connect(d->ui.horizontalslider, &QSlider::valueChanged, this, [this] (int value) {
+        d->ui.horizontalslider2->setValue(value);
+        d->ui.verticalslider->setValue(value);
+        d->ui.verticalslider2->setValue(value);
+    });
+    connect(d->ui.verticalslider, &QSlider::valueChanged, this, [this] (int value) {
+        d->ui.horizontalslider->setValue(value);
+        d->ui.horizontalslider2->setValue(value);
+        d->ui.verticalslider2->setValue(value);
+    });
 
     setMenuBar(new QMenuBar);
     QMenu *file = new QMenu("File");
