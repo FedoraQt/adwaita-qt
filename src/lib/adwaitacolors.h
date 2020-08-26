9e9b767891b57b7de38a4cbae1e8c8de7c75c5ee
Jan Grulich <jgrulich@redhat.com>
2020-08-26 11:14:55 +0200

    Colors: document relevant options for each method

--- src/lib/adwaitacolors.h
+++ src/lib/adwaitacolors.h
@@ -46,31 +46,165 @@ public:
     static QPalette palette(ColorVariant variant = ColorVariant::Unknown);
 
     // Generic colors
+
+    /* Relevant options:
+       * palette
+     */
     static QColor hoverColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+     */
     static QColor focusColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+     */
     static QColor negativeText(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+     */
     static QColor shadowColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * active
+     */
     static QColor titleBarColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * active
+     */
     static QColor titleBarTextColor(const StyleOptions &options);
 
     // Outline colors
+
+    /* Relevant options:
+       * palette
+       * color role
+       * color group
+     */
     static QColor arrowOutlineColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * mouse over
+       * has focus
+       * opacity
+       * animation mode
+       * color variant
+     */
     static QColor buttonOutlineColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * mouse over
+       * has focus
+       * opacity
+       * animation mode
+       * checkbox state
+       * color variant
+       * in menu
+     */
     static QColor indicatorOutlineColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * mouse over
+       * has focus
+       * opacity
+       * animation mode
+       * color variant
+     */
     static QColor frameOutlineColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * mouse over
+       * has focus
+       * opacity
+       * animation mode
+       * color variant
+     */
     static QColor inputOutlineColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * has focus
+       * opacity
+       * animation mode
+     */
     static QColor sidePanelOutlineColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * mouse over
+       * has focus
+       * opacity
+       * animation mode
+     */
     static QColor sliderOutlineColor(const StyleOptions &options);
 
     // Background colors
+
+    /* Relevant options:
+       * palette
+       * mouse over
+       * has focus
+       * sunken
+       * opacity
+       * animation mode
+       * color variant
+     */
     static QColor buttonBackgroundColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * mouse over
+       * active
+       * opacity
+       * animation mode
+       * color variant
+     */
     static QColor checkBoxIndicatorColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * state
+     */
     static QColor headerTextColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * mouse over
+       * has focus
+       * sunken
+       * opacity
+       * animation mode
+       * checkbox state
+       * color variant
+       * in menu
+     */
     static QColor indicatorBackgroundColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+     */
     static QColor frameBackgroundColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * mouse over
+       * has focus
+       * sunken
+       * opacity
+       * animation mode
+       * color variant
+     */
     static QColor scrollBarHandleColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * color variant
+     */
     static QColor separatorColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+       * mouse over
+       * has focus
+       * sunken
+       * opacity
+       * animation mode
+     */
     static QColor toolButtonColor(const StyleOptions &options);
+    /* Relevant options:
+       * palette
+     */
     static QColor tabBarColor(const StyleOptions &options);
 };
 
